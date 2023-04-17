e46a4915e637e03bce2ac320f85836ab12a18853
DavidLara004 <130525465+DavidLara004@users.noreply.github.com>
2023-04-17 16:32:23 -0500

    Segunda version
    #include <stdio.h>
    int main()
    {
        //num1 es la variable destinada al primer numero ingresado,num2 es la variable destinada al segundo numero ingresado
        int num1,num2,mayor,menor,i;
        printf("-------------Bienvenido--------- \n");
        printf("Ingrese el primer numero entero:");
        scanf("%d", &num1);
        printf("Ingrese el segundo numero entero:");
        scanf("%d", &num2);
        if ( num1 > num2 + 1 ){
        mayor=num1;
        menor=num2;
            printf("La cantidad de numeros entre %d y %d en un orden de menor a mayor son :  \n ", num1, num2);
        for (i=menor + 1; i<=mayor - 1; i++){
            printf( "%d ", i);
        }
        }
        if ( num2 > num1 + 1 ){
        mayor=num2;
        menor=num1;
            printf("La cantidad de numeros entre %d y %d en un orden de menor a mayor son :  \n ", num1, num2);
        for (i=menor + 1; i<=mayor - 1; i++){
            printf( "%d ", i);
        }
        }
        if ( num1 == num2 || num1 == num2 + 1 || num2 == num1 + 1 ){
        printf("No existen numeros entre los valores ingresados \n");
        }
        return 0;
    }

--- Ejercicios 16-4-23/ejercicio4.c
+++ Ejercicios 16-4-23/ejercicio4.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 int main()
 {
+    //num1 es la variable destinada al primer numero ingresado,num2 es la variable destinada al segundo numero ingresado
     int num1,num2,mayor,menor,i;
     printf("-------------Bienvenido--------- \n");
     printf("Ingrese el primer numero entero:");
     scanf("%d", &num1);
     printf("Ingrese el segundo numero entero:");
     scanf("%d", &num2);
-    if ( num1 > num2 ){
+    if ( num1 > num2 + 1 ){
     mayor=num1;
     menor=num2;
-    }
-    if ( num2 > num1 ){
-    mayor=num2;
-    menor=num1;
-    }
-    printf("La cantidad de numeros entre %d y %d en un orden de menor a mayor son :  \n ", num1, num2);
+        printf("La cantidad de numeros entre %d y %d en un orden de menor a mayor son :  \n ", num1, num2);
     for (i=menor + 1; i<=mayor - 1; i++){
         printf( "%d ", i);
-    
-  }
+    }
+    }
+    if ( num2 > num1 + 1 ){
+    mayor=num2;
+    menor=num1;
+        printf("La cantidad de numeros entre %d y %d en un orden de menor a mayor son :  \n ", num1, num2);
+    for (i=menor + 1; i<=mayor - 1; i++){
+        printf( "%d ", i);
+    }
+    }
+    if ( num1 == num2 || num1 == num2 + 1 || num2 == num1 + 1 ){
+    printf("No existen numeros entre los valores ingresados \n");
+    }
     return 0;
 }
-
