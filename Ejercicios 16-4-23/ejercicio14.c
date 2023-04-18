84bdac8e01254208ce06a97dd83d2888e1c311a5
DavidLara004 <130525465+DavidLara004@users.noreply.github.com>
2023-04-18 09:19:43 -0500

    Tercera versión (comentada)

--- Ejercicios 16-4-23/ejercicio14.c
+++ Ejercicios 16-4-23/ejercicio14.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int i,sum;
+    // Las variables utilizadas son i para el ciclo For (para el control del ciclo) y la variable mul que es la variable que saca el resultado de la multiplicación 
+    int i,mul;
     printf("-------------Bienvenido--------- \n");
     printf("Los primeros 20 multiplos de 3 son:\n");
+    // Ciclo For en el cual se utiliza se multiplica el numero tres por la variable i en cada iteracion. El resultado de esto da la variable multiplicacion.
     for(i=1; i<= 20; i++){
-    sum=3*i;
+    mul=3*i;
     printf("3 x %d = %d \n", i,sum);
     }
     return 0;
