9f4f49b785b410d40c482aed4d6df27031be580c
DavidLara004 <130525465+DavidLara004@users.noreply.github.com>
2023-04-18 21:39:03 -0500

    Update ejercicio34.c

--- Ejercicios 16-4-23/ejercicio34.c
+++ Ejercicios 16-4-23/ejercicio34.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
+    // Variable i utilizada para ciclo For que controla las iteraciones.
     int i;
     printf("-------------Bienvenido--------- \n");
     printf("Los numeros del 1 al 10 en orden descendente son:\n");
+    // Se utiliza ciclo For para que vaya imprimiendo el numero menor al anterior, hasta llegar al 1.
     for (i=10; i>=1; i--){
-        printf( "%d ", i);
-    
+        printf( "%d ", i); 
   }
     return 0;
 }
