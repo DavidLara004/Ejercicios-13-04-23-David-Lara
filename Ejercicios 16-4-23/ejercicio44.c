b57522a0563179495313f00de179242a23df6a1d
DavidLara004 <130525465+DavidLara004@users.noreply.github.com>
2023-04-18 21:57:23 -0500

    Tercera version (comentada)

--- Ejercicios 16-4-23/ejercicio44.c
+++ Ejercicios 16-4-23/ejercicio44.c
@@ -2,11 +2,12 @@
 
 int main()
 {
+  // num es la variable que lee el valor ingresado por el usuario , i la variable que controla las iteraciones en el ciclo For y fact es una variable que guarda la multiplicacion el numero 1 por los numeros anteriores al valor ingresado por el usuario
   int num, i, fact = 1;
   printf("-------------Bienvenido--------- \n");
   printf("Escribe un numero para calcular su factorial\n");
   scanf("%d", &num);
-
+// Se usa ciclo For para que se vaya iterando el numero en orden descendente y la variable fact multiplique el numero de la iteracion por el valor que tenga en ese momento la variable fact (ese numero sale de la anterior iteracion)
   for (i = num; i > 1; i--){
     fact = fact * i;
 }
